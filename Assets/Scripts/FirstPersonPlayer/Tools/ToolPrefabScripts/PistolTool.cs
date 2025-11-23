c3db7a2b77a340b08762b53caf1e85eda8f19aea
Ben Shapiro <bshap93@gmail.com>
2025-11-23 15:26:03 -0700

    Pistol type ranged

---
+++ Assets/Scripts/FirstPersonPlayer/Tools/ToolPrefabScripts/PistolTool.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace FirstPersonPlayer.Tools.ToolPrefabScripts
+{
+    public class PistolTool : RangedToolPrefab
+    {
+        public override void Initialize(PlayerEquipment owner)
+        {
+            throw new NotImplementedException();
+        }
+        public override void PerformToolAction()
+        {
+            throw new NotImplementedException();
+        }
+        public override void ApplyHit()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Assets/Scripts/FirstPersonPlayer/Tools/ToolPrefabScripts/RangedToolPrefab.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using FirstPersonPlayer.Tools.Interface;
+using Helpers.AnimancerHelper;
+using Helpers.Events.ManagerEvents;
+using MoreMountains.Feedbacks;
+using UnityEngine;
+
+namespace FirstPersonPlayer.Tools.ToolPrefabScripts
+{
+    public abstract class RangedToolPrefab : MonoBehaviour, IRuntimeTool
+    {
+        protected AnimancerRightArmController AnimancerRightArmController;
+        protected RaycastHit LastHit;
+        public abstract void Initialize(PlayerEquipment owner);
+        public void Use()
+        {
+            PerformToolAction();
+        }
+        public void Unequip()
+        {
+            throw new NotImplementedException();
+        }
+        public bool CanInteractWithObject(GameObject colliderGameObject)
+        {
+            throw new NotImplementedException();
+        }
+        public Sprite GetReticleForTool(GameObject colliderGameObject)
+        {
+            throw new NotImplementedException();
+        }
+        public bool CanAbortAction()
+        {
+            throw new NotImplementedException();
+        }
+        public MMFeedbacks GetEquipFeedbacks()
+        {
+            throw new NotImplementedException();
+        }
+        public CanBeAreaScannedType GetDetectableType()
+        {
+            throw new NotImplementedException();
+        }
+        public MMFeedbacks GetUnequipFeedbacks()
+        {
+            throw new NotImplementedException();
+        }
+
+        public abstract void PerformToolAction();
+
+        public abstract void ApplyHit();
+    }
+}
