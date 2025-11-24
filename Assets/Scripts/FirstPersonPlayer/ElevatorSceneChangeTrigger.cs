0f899093ec5b91e8fbf354cd7e3133fa4209870c
Ben Shapiro <bshap93@gmail.com>
2025-11-24 16:02:02 -0700

    Tutorial put away

--- Assets/Scripts/FirstPersonPlayer/ElevatorSceneChangeTrigger.cs
+++ Assets/Scripts/FirstPersonPlayer/ElevatorSceneChangeTrigger.cs
@@ -2,6 +2,7 @@ using System.Collections.Generic;
 using Helpers.Events;
 using Manager;
 using Sirenix.OdinInspector;
+using Structs;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.Serialization;
@@ -27,10 +28,10 @@ namespace FirstPersonPlayer
                 SaveDataEvent.Trigger();
 
                 // SpawnEvent.Trigger(
-                //     SpawnEventType.ToCaverns, spawnInfo.SceneName, GameMode.FirstPerson,
-                //     spawnInfo.SpawnPointId
+                //     SpawnEventType.ToMine, sceneToLoad, GameMode.FirstPerson,
+                //     spawnPointId
                 // );
-
+                //
                 SceneManager.LoadScene(bridgeName);
             }
         }
--- Assets/Scripts/PhysicsHandlers/Triggers/CollisionAlertTrigger.cs
+++ Assets/Scripts/PhysicsHandlers/Triggers/CollisionAlertTrigger.cs
@@ -1,9 +1,12 @@
+using System;
 using Helpers.Events;
+using MoreMountains.Tools;
 using UnityEngine;
+using Utilities.Interface;
 
 namespace PhysicsHandlers.Triggers
 {
-    public class CollisionAlertTrigger : MonoBehaviour
+    public class CollisionAlertTrigger : MonoBehaviour, MMEventListener<SpontaneousTriggerEvent>, IRequiresUniqueID
     {
         [SerializeField] string uniqueID;
         [SerializeField] string alertMessage;
@@ -14,16 +17,41 @@ namespace PhysicsHandlers.Triggers
         // Start is called once before the first execution of Update after the MonoBehaviour is created
         void Start()
         {
+            alertTriggerCollider.SetActive(false);
         }
 
         // Update is called once per frame
         void Update()
         {
         }
+        void OnEnable()
+        {
+            this.MMEventStartListening();
+        }
+        void OnDisable()
+        {
+            this.MMEventStopListening();
+        }
+        public string UniqueID => uniqueID;
+        public void SetUniqueID()
+        {
+            uniqueID = Guid.NewGuid().ToString();
+        }
+        public bool IsUniqueIDEmpty()
+        {
+            return string.IsNullOrEmpty(uniqueID);
+        }
+        public void OnMMEvent(SpontaneousTriggerEvent eventType)
+        {
+            if (eventType.UniqueID == uniqueID) EnableAlertTriggerCollider();
+        }
         public void TriggerAlert()
         {
             AlertEvent.Trigger(alertReason, alertMessage, alertTitle, alertType);
-            alertTriggerCollider.SetActive(false);
+        }
+        public void EnableAlertTriggerCollider()
+        {
+            alertTriggerCollider.SetActive(true);
         }
     }
 }
