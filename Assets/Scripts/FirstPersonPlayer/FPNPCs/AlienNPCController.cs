dd4cbbf3b90759f87b42b9b943352411d84147a1
Ben Shapiro <bshap93@gmail.com>
2025-11-25 07:46:20 -0700

    Good vibration

---
+++ Assets/Scripts/FirstPersonPlayer/FPNPCs/AlienNPCController.cs
@@ -0,0 +1,14 @@
+﻿using Animancer;
+using MoreMountains.Feedbacks;
+using UnityEngine;
+
+namespace FirstPersonPlayer.FPNPCs
+{
+    public class AlienNPCController : MonoBehaviour
+    {
+        [SerializeField] AnimancerComponent animancerComponent;
+        [SerializeField] string NPCId;
+        [SerializeField] string defaultStartNode;
+        [SerializeField] MMFeedbacks startDialogueFeedback;
+    }
+}
