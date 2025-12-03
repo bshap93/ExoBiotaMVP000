64d319640cd24ec2a28d4646febe98179a41bee7
Ben Shapiro <bshap93@gmail.com>
2025-12-03 11:43:20 -0700

    add cryobed

---
+++ Assets/Scripts/FirstPersonPlayer/Interactable/MediStatHub.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using Utilities.Interface;
+
+namespace FirstPersonPlayer.Interactable
+{
+    public class MediStatHub : MonoBehaviour, IRequiresUniqueID
+    {
+        public string uniqueID;
+        public string UniqueID => uniqueID;
+        public void SetUniqueID()
+        {
+            uniqueID = System.Guid.NewGuid().ToString();
+        }
+        public bool IsUniqueIDEmpty()
+        {
+            return string.IsNullOrEmpty(uniqueID);
+        }
+    }
+}
---
+++ Assets/Scripts/Manager/AttributesManager.cs
@@ -0,0 +1,47 @@
+﻿using Helpers;
+using Helpers.Interfaces;
+using MoreMountains.Tools;
+using UnityEngine;
+
+namespace Manager
+{
+    public class AttributesManager : MonoBehaviour, ICoreGameService, MMEventListener<AttributeEvent>
+    {
+        public void Save()
+        {
+            throw new System.NotImplementedException();
+        }
+        public void Load()
+        {
+            throw new System.NotImplementedException();
+        }
+        public void Reset()
+        {
+            throw new System.NotImplementedException();
+        }
+        public void ConditionalSave()
+        {
+            throw new System.NotImplementedException();
+        }
+        public void MarkDirty()
+        {
+            throw new System.NotImplementedException();
+        }
+        public string GetSaveFilePath()
+        {
+            throw new System.NotImplementedException();
+        }
+        public void CommitCheckpointSave()
+        {
+            throw new System.NotImplementedException();
+        }
+        public bool HasSavedData()
+        {
+            throw new System.NotImplementedException();
+        }
+        public void OnMMEvent(AttributeEvent eventType)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
