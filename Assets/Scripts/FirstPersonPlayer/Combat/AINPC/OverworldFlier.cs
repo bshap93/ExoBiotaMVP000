0166fde27b792b56d9b1c8254fd89f27e288e7c3
Ben Shapiro <bshap93@gmail.com>
2025-12-06 14:18:30 -0700

    New branch first new commit

---
+++ Assets/Scripts/FirstPersonPlayer/Combat/AINPC/OverworldFlier.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using Animancer;
+using NodeCanvas.Framework;
+using UnityEngine;
+using UnityEngine.AI;
+using Utilities.Interface;
+
+namespace FirstPersonPlayer.Combat.AINPC
+{
+    [RequireComponent(typeof(Blackboard))]
+    [RequireComponent(typeof(AnimancerComponent))]
+    [DisallowMultipleComponent]
+    public class OverworldFlierController : MonoBehaviour, IRequiresUniqueID
+    {
+        public string uniqueID;
+
+        [SerializeField] NavMeshAgent navMeshAgent;
+        [SerializeField] AnimancerComponent animancerComponent;
+        [SerializeField] Blackboard blackboard;
+
+        AnimancerState idleState;
+        AnimancerState moveState;
+        public string UniqueID => uniqueID;
+        public void SetUniqueID()
+        {
+            uniqueID = Guid.NewGuid().ToString();
+        }
+        public bool IsUniqueIDEmpty()
+        {
+            return string.IsNullOrEmpty(uniqueID);
+        }
+    }
+}
