3b337785e56a196b6ad93fed367742b58f66a39a
Ben Shapiro <bshap93@gmail.com>
2025-12-06 12:29:15 -0700

    Air beast

---
+++ Assets/Scripts/Dirigible/NPCs/OverworldFlierController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using Animancer;
+using NodeCanvas.Framework;
+using UnityEngine;
+using UnityEngine.AI;
+using Utilities.Interface;
+
+namespace Dirigible.NPCs
+{
+    [RequireComponent(typeof(Blackboard))]
+    [RequireComponent(typeof(NavMeshAgent))]
+    [RequireComponent(typeof(AnimancerComponent))]
+    [DisallowMultipleComponent]
+    public class OverworldFlierController : MonoBehaviour, IRequiresUniqueID
+    {
+        public string uniqueID;
+
+        [SerializeField] NavMeshAgent navMeshAgent;
+        [SerializeField] AnimancerComponent animancerComponent;
+        [SerializeField] Blackboard blackboard;
+
+        AnimancerState idleState;
+        AnimancerState moveState;
+        public string UniqueID => uniqueID;
+        public void SetUniqueID()
+        {
+            uniqueID = Guid.NewGuid().ToString();
+        }
+        public bool IsUniqueIDEmpty()
+        {
+            return string.IsNullOrEmpty(uniqueID);
+        }
+    }
+}
