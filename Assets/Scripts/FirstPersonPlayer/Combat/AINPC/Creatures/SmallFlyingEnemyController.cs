ad29fd2f9b1be81e89ae71cac5e275d6d58de967
Ben Shapiro <bshap93@gmail.com>
2025-12-08 11:55:35 -0700

    Wing bug

---
+++ Assets/Scripts/FirstPersonPlayer/Combat/AINPC/Creatures/SmallFlyingEnemyController.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+
+namespace FirstPersonPlayer.Combat.AINPC.Creatures
+{
+    public class SmallFlyingEnemyController : EnemyController
+    {
+        [SerializeField] GameObject physicalCreatureGameObject;
+    }
+}
