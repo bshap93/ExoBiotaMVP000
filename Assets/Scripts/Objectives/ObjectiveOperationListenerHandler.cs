df04469d17b1c5b99070920591d3f0ed54c5fa66
Ben Shapiro <bshap93@gmail.com>
2025-12-10 13:24:20 -0700

    Add objective to decontaminate

--- Assets/Scripts/Objectives/ObjectiveOperationListenerHandler.cs
+++ Assets/Scripts/Objectives/ObjectiveOperationListenerHandler.cs
@@ -1,5 +1,6 @@
 using Events;
 using Helpers.Events.Gated;
+using Helpers.Events.Status;
 using MoreMountains.Tools;
 using Objectives.ScriptableObjects;
 using UnityEngine;
@@ -7,9 +8,10 @@ using UnityEngine;
 namespace Objectives
 {
     public class ObjectiveOperationListenerHandler : MonoBehaviour, MMEventListener<ObjectiveEvent>,
-        MMEventListener<GatedLevelingEvent>
+        MMEventListener<GatedLevelingEvent>, MMEventListener<PlayerStatsEvent>
     {
         [SerializeField] ObjectiveObject objectiveToCompleteWhenGatedLevelingEventOccurs;
+        [SerializeField] ObjectiveObject objectiveToCompleteWhenDecontaminationTakesPlace;
         ObjectivesManager _objectivesManager;
 
         void Awake()
@@ -20,12 +22,14 @@ namespace Objectives
         {
             this.MMEventStartListening<ObjectiveEvent>();
             this.MMEventStartListening<GatedLevelingEvent>();
+            this.MMEventStartListening<PlayerStatsEvent>();
         }
 
         void OnDisable()
         {
             this.MMEventStopListening<ObjectiveEvent>();
             this.MMEventStopListening<GatedLevelingEvent>();
+            this.MMEventStopListening<PlayerStatsEvent>();
         }
         public void OnMMEvent(GatedLevelingEvent eventType)
         {
@@ -36,6 +40,12 @@ namespace Objectives
 
         public void OnMMEvent(ObjectiveEvent e)
         {
+        }
+        public void OnMMEvent(PlayerStatsEvent eventType)
+        {
+            if (eventType.ChangeType == PlayerStatsEvent.PlayerStatChangeType.Decrease &&
+                eventType.StatType == PlayerStatsEvent.PlayerStat.CurrentContamination)
+                _objectivesManager.CompleteObjective(objectiveToCompleteWhenDecontaminationTakesPlace.objectiveId);
         }
     }
 }
