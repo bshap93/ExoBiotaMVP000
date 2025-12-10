0a5bd13e75defd7b466ef4b1d1b9c6062189324c
Ben Shapiro <bshap93@gmail.com>
2025-12-10 14:52:49 -0700

    Commit before PUTTING ON GAMETESTER 12.10.25

--- Assets/Scripts/CamerasD/BoxVolumeListener.cs
+++ Assets/Scripts/CamerasD/BoxVolumeListener.cs
@@ -8,139 +8,144 @@ using UnityEngine.SceneManagement;
 using UnityEngine.Serialization;
 using Volume = UnityEngine.Rendering.Volume;
 
-public class BoxVolumeListener : MonoBehaviour, MMEventListener<GlobalSettingsEvent>,
-    MMEventListener<LoadedManagerEvent>, MMEventListener<StatsStatusEvent>,
-    MMEventListener<VisionAffectingStatusEffEvent>
+namespace CamerasD
 {
-    [FormerlySerializedAs("volume")] [FormerlySerializedAs("_volume")] [SerializeField]
-    Volume mainVolume;
-    [SerializeField] Volume distortVolume01;
-    [SerializeField] Volume floatersVolume01;
-
-    public VolumeProfile ditheringProfile;
-    public VolumeProfile cleanProfile;
-
-    public VolumeProfile initialDistortProfile;
-
-    public VolumeProfile distortProfile;
-    public VolumeProfile floatersProfile;
-    VolumeProfile _runtimeProfile;
-    public BoxVolumeListener(VolumeProfile initialDistortProfile)
+    public class BoxVolumeListener : MonoBehaviour, MMEventListener<GlobalSettingsEvent>,
+        MMEventListener<LoadedManagerEvent>, MMEventListener<StatsStatusEvent>,
+        MMEventListener<VisionAffectingStatusEffEvent>
     {
-        this.initialDistortProfile = initialDistortProfile;
-    }
+        [FormerlySerializedAs("volume")] [FormerlySerializedAs("_volume")] [SerializeField]
+        Volume mainVolume;
+        [SerializeField] Volume distortVolume01;
+        [SerializeField] Volume floatersVolume01;
+        [SerializeField] bool disableNonDithering;
 
-    void Awake()
-    {
-        // Clone the original profile so you don't modify the asset
-        if (mainVolume.profile != null)
+        public VolumeProfile ditheringProfile;
+        // public VolumeProfile cleanProfile;
+
+        public VolumeProfile initialDistortProfile;
+
+        public VolumeProfile distortProfile;
+        public VolumeProfile floatersProfile;
+        VolumeProfile _runtimeProfile;
+        public BoxVolumeListener(VolumeProfile initialDistortProfile)
         {
-            _runtimeProfile = Instantiate(mainVolume.profile);
-            mainVolume.profile = _runtimeProfile;
+            this.initialDistortProfile = initialDistortProfile;
         }
-    }
 
-
-    void OnEnable()
-    {
-        this.MMEventStartListening<GlobalSettingsEvent>();
-        this.MMEventStartListening<LoadedManagerEvent>();
-        this.MMEventStartListening<StatsStatusEvent>();
-        this.MMEventStartListening<VisionAffectingStatusEffEvent>();
-
-        SceneManager.sceneLoaded -= OnSceneLoaded;
-
-        SceneManager.sceneLoaded += OnSceneLoaded;
-    }
-
-
-    void OnDisable()
-    {
-        this.MMEventStopListening<GlobalSettingsEvent>();
-        this.MMEventStopListening<LoadedManagerEvent>();
-        this.MMEventStopListening<StatsStatusEvent>();
-        this.MMEventStopListening<VisionAffectingStatusEffEvent>();
-
-        SceneManager.sceneLoaded -= OnSceneLoaded;
-    }
-    public void OnMMEvent(GlobalSettingsEvent eventType)
-    {
-        if (eventType.EventType == GlobalSettingsEventType.DitheringToggled)
+        void Awake()
         {
-            SetDithering(eventType.ChoiceIndex == 1);
-            Debug.Log("Dithering set to " + (eventType.ChoiceIndex == 1));
+            // Clone the original profile so you don't modify the asset
+            if (mainVolume.profile != null)
+            {
+                _runtimeProfile = Instantiate(mainVolume.profile);
+                mainVolume.profile = _runtimeProfile;
+            }
         }
-    }
-    public void OnMMEvent(LoadedManagerEvent eventType)
-    {
-        if (eventType.ManagerType == ManagerType.All)
+
+
+        void OnEnable()
+        {
+            this.MMEventStartListening<GlobalSettingsEvent>();
+            this.MMEventStartListening<LoadedManagerEvent>();
+            this.MMEventStartListening<StatsStatusEvent>();
+            this.MMEventStartListening<VisionAffectingStatusEffEvent>();
+
+            SceneManager.sceneLoaded -= OnSceneLoaded;
+
+            SceneManager.sceneLoaded += OnSceneLoaded;
+        }
+
+
+        void OnDisable()
+        {
+            this.MMEventStopListening<GlobalSettingsEvent>();
+            this.MMEventStopListening<LoadedManagerEvent>();
+            this.MMEventStopListening<StatsStatusEvent>();
+            this.MMEventStopListening<VisionAffectingStatusEffEvent>();
+
+            SceneManager.sceneLoaded -= OnSceneLoaded;
+        }
+        public void OnMMEvent(GlobalSettingsEvent eventType)
+        {
+            if (eventType.EventType == GlobalSettingsEventType.DitheringToggled)
+            {
+                SetDithering(eventType.ChoiceIndex == 1);
+                Debug.Log("Dithering set to " + (eventType.ChoiceIndex == 1));
+            }
+        }
+        public void OnMMEvent(LoadedManagerEvent eventType)
+        {
+            if (eventType.ManagerType == ManagerType.All)
+            {
+                var gsm = GlobalSettingsManager.Instance;
+                if (gsm != null)
+                    SetDithering(gsm.DitheringEnabled);
+            }
+        }
+        public void OnMMEvent(StatsStatusEvent eventType)
+        {
+            switch (eventType.StatType)
+            {
+                case StatsStatusEvent.StatsStatusType.Contamination:
+                    HandleContaminationStatus(eventType.Enabled, eventType.Status);
+                    break;
+            }
+        }
+        public void OnMMEvent(VisionAffectingStatusEffEvent eventType)
+        {
+            if (eventType.Enable)
+                switch (eventType.StatusEffType)
+                {
+                    case VisionAffectingStatusEffType.Distortion:
+                        distortVolume01.profile = distortProfile;
+                        distortVolume01.enabled = true;
+                        break;
+                    case VisionAffectingStatusEffType.Floaters:
+                        floatersVolume01.profile = floatersProfile;
+                        floatersVolume01.enabled = true;
+                        break;
+                }
+            else
+                switch (eventType.StatusEffType)
+                {
+                    case VisionAffectingStatusEffType.Distortion:
+                        distortVolume01.enabled = false;
+                        break;
+                    case VisionAffectingStatusEffType.Floaters:
+                        floatersVolume01.enabled = false;
+                        break;
+                    case VisionAffectingStatusEffType.All:
+                        distortVolume01.enabled = false;
+                        floatersVolume01.enabled = false;
+                        break;
+                }
+        }
+
+        void HandleContaminationStatus(bool isBeingEnabled, StatsStatusEvent.StatsStatus eventTypeStatus)
+        {
+            switch (eventTypeStatus)
+            {
+                case StatsStatusEvent.StatsStatus.IsMax:
+                    // SetDistortion(isBeingEnabled);
+                    break;
+            }
+        }
+
+        void OnSceneLoaded(Scene scene, LoadSceneMode mode)
         {
             var gsm = GlobalSettingsManager.Instance;
             if (gsm != null)
                 SetDithering(gsm.DitheringEnabled);
         }
-    }
-    public void OnMMEvent(StatsStatusEvent eventType)
-    {
-        switch (eventType.StatType)
+
+        void SetDithering(bool ditheringEnabled)
         {
-            case StatsStatusEvent.StatsStatusType.Contamination:
-                HandleContaminationStatus(eventType.Enabled, eventType.Status);
-                break;
+            if (disableNonDithering) return;
+            if (ditheringEnabled)
+                mainVolume.profile = ditheringProfile;
+            // else
+            //     mainVolume.profile = cleanProfile;
         }
-    }
-    public void OnMMEvent(VisionAffectingStatusEffEvent eventType)
-    {
-        if (eventType.Enable)
-            switch (eventType.StatusEffType)
-            {
-                case VisionAffectingStatusEffType.Distortion:
-                    distortVolume01.profile = distortProfile;
-                    distortVolume01.enabled = true;
-                    break;
-                case VisionAffectingStatusEffType.Floaters:
-                    floatersVolume01.profile = floatersProfile;
-                    floatersVolume01.enabled = true;
-                    break;
-            }
-        else
-            switch (eventType.StatusEffType)
-            {
-                case VisionAffectingStatusEffType.Distortion:
-                    distortVolume01.enabled = false;
-                    break;
-                case VisionAffectingStatusEffType.Floaters:
-                    floatersVolume01.enabled = false;
-                    break;
-                case VisionAffectingStatusEffType.All:
-                    distortVolume01.enabled = false;
-                    floatersVolume01.enabled = false;
-                    break;
-            }
-    }
-
-    void HandleContaminationStatus(bool isBeingEnabled, StatsStatusEvent.StatsStatus eventTypeStatus)
-    {
-        switch (eventTypeStatus)
-        {
-            case StatsStatusEvent.StatsStatus.IsMax:
-                // SetDistortion(isBeingEnabled);
-                break;
-        }
-    }
-
-    void OnSceneLoaded(Scene scene, LoadSceneMode mode)
-    {
-        var gsm = GlobalSettingsManager.Instance;
-        if (gsm != null)
-            SetDithering(gsm.DitheringEnabled);
-    }
-
-    void SetDithering(bool ditheringEnabled)
-    {
-        if (ditheringEnabled)
-            mainVolume.profile = ditheringProfile;
-        else
-            mainVolume.profile = cleanProfile;
     }
 }
