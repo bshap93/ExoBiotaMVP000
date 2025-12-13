23e32d23cd63a106f48a1cdf5b515f64af28310a
Ben Shapiro <bshap93@gmail.com>
2025-12-13 15:52:13 -0700

    Top up saturday

---
+++ Assets/Scripts/FirstPersonPlayer/Tools/ToolPrefabScripts/Weapon/RangedPistolWeapon.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using UnityEngine;
+
+namespace FirstPersonPlayer.Tools.ToolPrefabScripts.Weapon
+{
+    public class RangedPistolWeapon : RangedToolPrefab
+    {
+        [SerializeField] GameObject slider;
+        [SerializeField] GameObject frontEmitter;
+        [SerializeField] GameObject cell;
+        [SerializeField] GameObject trigger;
+        public override void Initialize(PlayerEquipment owner)
+        {
+            throw new NotImplementedException();
+        }
+        public override void PerformToolAction()
+        {
+            throw new NotImplementedException();
+        }
+        public override void ApplyHit()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
