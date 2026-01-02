a17151ab394543ef4f7920500b5f8329bc6939a3
Ben Shapiro <bshap93@gmail.com>
2026-01-02 13:57:14 -0700

    Refine Hotbar

--- Assets/Scripts/SharedUI/Hotbar/HotbarUISlot.cs
+++ Assets/Scripts/SharedUI/Hotbar/HotbarUISlot.cs
@@ -1,10 +1,32 @@
+using Sirenix.OdinInspector;
+using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 namespace SharedUI.Hotbar
 { 
     [DisallowMultipleComponent]
     public class HotbarUISlot : MonoBehaviour
     {
+        public enum HotbarSlotType
+        {
+            Tool,
+            Hands,
+            EmptySlot,
+            Consumable,
+        }
+        [SerializeField] Image slotBG;
+        [SerializeField] Image secondaryBG;
+        [SerializeField] Image itemIcon;
+        [SerializeField] bool quantityEnabled;
+        [ShowIf("quantityEnabled")]
+        [SerializeField] TMP_Text quantityText;
+        [ShowIf("quantityEnabled")]
+        [SerializeField] GameObject quantityBadge;
+        [ShowIf("quantityEnabled")]
+        [SerializeField] Color moreThanZeroColor;
+        [ShowIf("quantityEnabled")]
+        [SerializeField] Color zeroColor;
         // Start is called once before the first execution of Update after the MonoBehaviour is created
         void Start()
         {
